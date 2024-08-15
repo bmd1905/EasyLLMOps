f832b48918aca907a6d3ed4bcc65ffb17156891a
Minh-Duc Bui <minhduc2k31905@gmail.com>
2024-08-15 17:09:12 +0700

    ✨ feat(monitor): Add monitoring tools
    - Tools: Grafana, Prometheus, Jaeger, Alermanager

--- api/src/routers/conversation.py
+++ api/src/routers/conversation.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import StreamingResponse
+from opentelemetry import trace
 
 from src import logger
 from src.configs.prompt_config.prompt_loader import PROMPT_TEMPLATES, SYSTEM_PROMPTS
+from src.dependencies import counter, histogram, tracer
 from src.llms.litellm import generate_response_non_stream, generate_response_stream
 from src.schemas.prompt_schema import ConversationIn
 
@@ -27,7 +29,7 @@ async def enhance_prompt(prompt_type: str, latest_prompt: str) -> str:
     return enhanced_prompt.final_prompt
 
 
-async def generate_streaming_response(final_prompt: str, history: list):
+async def handle_streaming_response(final_prompt: str, history: list):
     """Generates a streaming response using the specified language model."""
 
     async def stream_generator():
@@ -46,7 +48,7 @@ async def generate_streaming_response(final_prompt: str, history: list):
     return StreamingResponse(stream_generator(), media_type='text/event-stream')
 
 
-async def generate_non_streaming_response(final_prompt: str, history: list):
+async def handle_non_streaming_response(final_prompt: str, history: list):
     """Generates a non-streaming response using the specified language model."""
     response = await generate_response_non_stream(
         prompt=final_prompt,
@@ -73,31 +75,55 @@ async def conversation_endpoint(data: ConversationIn):
 
     :raises HTTPException: If an error occurs during the conversation processing.
     """
-    # Parse the request data
-    prompt_type = data.prompt_type
-    message = data.message
-    history = data.history
-    stream = data.stream
-    latest_prompt = data.latest_prompt
+    with tracer.start_as_current_span('processors') as processors:
+        # Parse the request data
+        prompt_type = data.prompt_type
+        message = data.message
+        history = data.history
+        stream = data.stream
+        latest_prompt = data.latest_prompt
 
-    try:
-        # Load and format the prompt
-        formatted_latest_prompt = PROMPT_TEMPLATES[prompt_type].format(prompt=latest_prompt)
+        total_duration = 0  # Initialize total duration for histogram
 
-        # Enhance Prompt (Only for the first prompt)
-        if not history:
-            final_prompt = await enhance_prompt(prompt_type, formatted_latest_prompt)
-        else:
-            final_prompt = message  # Use the original message for subsequent prompts
+        try:
+            # Load and format the prompt
+            formatted_latest_prompt = PROMPT_TEMPLATES[prompt_type].format(prompt=latest_prompt)
 
-        # Generate Response
-        logger.info(f'Generating response with prompt: "{final_prompt}"')
+            # ----------------- Step 1: Enhance Prompt -----------------
+            with tracer.start_as_current_span(
+                'step-1-enhance_prompt',
+                links=[trace.Link(processors.get_span_context())],
+            ):
+                if not history:
+                    final_prompt = await enhance_prompt(prompt_type, formatted_latest_prompt)
+                else:
+                    final_prompt = message  # Use the original message for subsequent prompts
 
-        if stream:
-            return await generate_streaming_response(final_prompt, history)
-        else:
-            return await generate_non_streaming_response(final_prompt, history)
+            # ----------------- Step 2: Generate Response -----------------
+            logger.info(f'Generating response with prompt: "{final_prompt}"')
 
-    except Exception as e:
-        logger.error(f'Error in conversation endpoint: {e}')
-        raise HTTPException(status_code=500, detail=f'An Error occurred: {str(e)}')
+            with tracer.start_as_current_span(
+                'step-2-generate_response',
+                links=[trace.Link(processors.get_span_context())],
+            ):
+                if stream:
+                    response = await handle_streaming_response(final_prompt, history)
+                else:
+                    response = await handle_non_streaming_response(final_prompt, history)
+
+            return response
+
+        except Exception as e:
+            logger.error(f'Error in conversation endpoint: {e}')
+            raise HTTPException(status_code=500, detail=f'An Error occurred: {str(e)}')
+
+        finally:
+            # ----------------- Metrics -----------------
+            # Labels for all metrics
+            label = {'api': '/api/promptalchemy_conversation/conversation'}
+
+            # Increase the counter
+            counter.add(1, label)
+
+            # Add histogram (recording duration would require actual duration calculation)
+            histogram.record(total_duration, label)
