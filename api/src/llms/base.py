22627c04d3da1f6e830aa3ce5fd21e796c9322ef
Minh-Duc Bui <90423581+bmd1905@users.noreply.github.com>
2024-08-15 11:42:11 +0700

    ✨ feat(api): Implement conversation functionality and a simple Gradio UI (#7)
    * ✨ feat(api): Implement conversation functionality
    
    * ✨ feat(ui): Add simple Gradio UI

--- api/src/llms/base.py
+++ api/src/llms/base.py
@@ -3,7 +3,7 @@ import openai
 
 from src.configs.litellm_config import litellm_config
 
-client = openai.OpenAI(
+client = openai.AsyncOpenAI(
     api_key='anything',
     base_url=litellm_config.LITELLM_PROXY_URL,
 )
