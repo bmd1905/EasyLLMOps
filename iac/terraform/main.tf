c4a464b7e0ac984d0cdc7c112e9afff53a677623
Minh-Duc Bui <minhduc2k31905@gmail.com>
2024-08-23 18:49:21 +0700

    🚀 feat(production): Set-up IaC (Ansible + Terraform)

---
+++ iac/terraform/main.tf
@@ -0,0 +1,38 @@
+# Ref: https://github.com/terraform-google-modules/terraform-google-kubernetes-engine/blob/master/examples/simple_autopilot_public
+# To define that we will use GCP
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.80.0" // Provider version
+    }
+  }
+  required_version = "1.9.5" // Terraform version
+}
+
+// The library with methods for creating and
+// managing the infrastructure in GCP, this will
+// apply to all the resources in the project
+provider "google" {
+  credentials = "./prompt-alchemy-1935af3ba5e8.json"
+  project = var.project_id
+  region  = var.region
+}
+
+// Google Kubernetes Engine
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.region
+
+  // Enabling Autopilot for this cluster
+  enable_autopilot = false
+
+  // Specify the initial number of nodes
+  initial_node_count = 1
+
+  // Node configuration
+  node_config {
+    machine_type = "e2-medium" // 2 vCPUs, 4 GB memory
+    disk_size_gb = 30
+  }
+}
